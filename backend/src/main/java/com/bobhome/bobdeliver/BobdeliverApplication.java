cb2b0be8c594764982da60ba43a83edf887c3344
Roberto Okamura <rob.okamura@gmail.com>
2022-08-15 19:02:54 -0300

    Project created

---
+++ backend/src/main/java/com/bobhome/bobdeliver/BobdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.bobhome.bobdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bobhome/bobdeliver/BobdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobhome.bobdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
